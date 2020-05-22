f0b981863f5bcedb64cbb92a664b419587c52d1a
Wim Jacops <wim.jacops@gmail.com>
2020-05-22 21:28:22 +0200

    Add new routes

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,18 @@ from . import main
 @main.route('/')
 def index():
     return render_template('base.html')
+
+
+@main.route('/domotica')
+def domotica():
+    return render_template('domotica.html')
+
+
+@main.route('/kookboek')
+def kookboek():
+    return render_template('kookboek.html')
+
+
+@main.route('/runner')
+def runner():
+    return render_template('runner.html')
