2d949dee2c4357d86a7134d72820840e14b75c2f
Wim Jacops <wim.jacops@gmail.com>
2020-07-16 22:41:07 +0200

    update test folder in manage.py

---
+++ manage_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+import pytest
+from flask_script import Manager, Shell
+from home_portal import create_app
+
+home_portal = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(home_portal)
+
+
+@manager.command
+def test():
+    pytest.main(["", "/home/homeportal/"])
+
+
+def make_shell_context():
+    return dict(app=home_portal)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
+
+if __name__ == '__main__':
+    manager.run()
