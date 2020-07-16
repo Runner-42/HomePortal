c1e2392d3e685ae9ae09de5b15b7297c7653566e
Wim Jacops <wim.jacops@gmail.com>
2020-07-16 19:56:03 +0200

    fix db initialisation issue

--- home_portal/__init__.py
+++ home_portal/__init__.py
@@ -17,6 +17,9 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config[config_name])
 
+    csrf.init_app(app)
+    db.init_app(app)
+
     with app.app_context():
 
         from .home import home
